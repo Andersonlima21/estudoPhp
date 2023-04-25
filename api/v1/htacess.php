45ff5389ac6d0be9e2c5085daf8f74c69f060f89
Anderson Leonardo <and.seijuro21@gmail.com>
2023-04-25 10:39:12 -0300

    feat: subindo estrutura

---
+++ api/v1/htacess.php
@@ -0,0 +1,7 @@
+// htacess para trabalhar com url amigavel
+
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^(.*)$ index.php?url=$1 [QSA,L]
+
